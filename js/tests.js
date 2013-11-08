1d08e556e76bae9296001936cc88afbfa38abe5b
FostiyRuslan <rfostiy@ettera.com>
2013-11-08 22:38:07 +0200

    show title when user set cursor over todo item

--- js/tests.js
+++ js/tests.js
@@ -2,15 +2,15 @@
 var testCtrl = TodosControllerForTests();
 
 describe("changeStatus test", function(){
-  it("check changeStatus result", function(){
-      var changeStatusResult = testCtrl.changeStatus(testCtrl.roles.admin);
-      expect(changeStatusResult.name).toBe("Admin");
-  });
+    it("check changeStatus result", function(){
+        var changeStatusResult = testCtrl.changeStatus(testCtrl.roles.admin);
+        expect(changeStatusResult.name).toBe("Admin");
+    });
 });
 
 describe("removeElement test", function(){
-  it("check rights before removeElement", function(){
-      var removeElementResult = testCtrl.removeTodo(testCtrl.roles.user);
-      expect(removeElementResult).toBe(false);
-  });
+    it("check rights before removeElement", function(){
+        var removeElementResult = testCtrl.removeTodo(testCtrl.roles.user);
+        expect(removeElementResult).toBe(false);
+    });
 });
