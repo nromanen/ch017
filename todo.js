dc26e3d68707ae3eb1eeefde27a76c343358ce79
Danylo <veneomin01@gmail.com>
2013-11-09 14:07:41 +0200

    Update todo.js
    aded js to radio box in todo.html

--- todo.js
+++ todo.js
@@ -1,51 +1,55 @@
-    var acsessLevel = 0; 
-	var btnvalue = 0;	
+    var acsessLevel = 'ROOT'; 
+	var btnValue = 'ADD';	
 		function TodoCtrl($scope) {
+		
+		
+		
 			$scope.todos = [
-					{text:'Simple text', done:false},
-					{text:'And this is also simple text', done:false},
-					{text:'And this', done:false},
-					{text:'This is line-through text', done:true}
-					];
+							{text:'Simple text', done:false},
+							{text:'And this is also simple text', done:false},
+							{text:'And this', done:false},
+							{text:'This is line-through text', done:true}
+							];
 	 
     $scope.addTodo = function() {
 	
-	if (acsessLevel === 1 || acsessLevel === 2 && btnvalue === 0 ){
+	if (acsessLevel === 'READ_CREATE' || acsessLevel === 'ROOT' && btnValue === 'ADD' ){
 		$scope.todos.push({text:$scope.todoText, done:false});
 		$scope.todoText = '';
 	}
 	
-	else if (acsessLevel === 1 || acsessLevel === 2 && btnvalue === 1 ) {
+	else if (acsessLevel === 'READ_CREATE' || acsessLevel === 'ROOT' && btnValue === 'EDIT' ) {
 		this.todos[index] = {text:$scope.todoText, done:false};
 		$scope.todoText = '';
-		btnvalue = 0;
+		btnValue = 'ADD';
 		document.getElementById('buttonSend').value = 'Add text';
 	}else{
 		alert('No enought acsess level to do this');
 	};
     } 
-    $scope.del = function(ToRemove) {	
-	if (acsessLevel !== 2){
-	alert('No enought acsess level to do this');
+    $scope.del = function(ToRemove) {
+	if (acsessLevel !== 'ROOT'){
+		alert('No enought acsess level to do this');
 	}else{
 		var index = this.todos.indexOf(ToRemove);
 		this.todos.splice(index, 1);
 	}
 	}
 	
-	$scope.edit = function(editme){
-	if (acsessLevel !== 2){
+	$scope.edit = function(editElem){
+	if (acsessLevel !== 'ROOT'){
 	alert('No enought acsess level to do this');
 	}else{
-		index = this.todos.indexOf(editme);
+		index = this.todos.indexOf(editElem);
 		$scope.todoText = this.todos[index].text;
 		document.getElementById('buttonSend').value = 'Edit text';
-		btnvalue = 1;
+		btnValue = 'EDIT';
 	}
 	}
 	
-	$scope.level0 = function() {acsessLevel = 0;}
-	$scope.level1 = function() {acsessLevel = 1;}
-	$scope.level2 = function() {acsessLevel = 2;}
+		$scope.go = function() {
+    	acsessLevel = $scope.acsessLvl;
+		}
+	
 		
     }
