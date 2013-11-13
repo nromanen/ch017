2671177ffabb7b3622a22b09f006197831cd8df9
Artur Dolgii <artur.dolgii@gmail.com>
2013-11-14 01:39:26 +0200

    Code coverage = 100%

--- tests/js/TodoCtrlTest.js
+++ tests/js/TodoCtrlTest.js
@@ -53,10 +53,95 @@ describe('TodosCtrl', function() {
         $rootScope.todoText = '';
         expect($rootScope.canAddTodo()).toBe(false);
     }));
-
-    it('check func for clear done todos', inject(function ($controller, $rootScope) {
+    
+    it('Should check rights to edit item', inject(function ($controller, $rootScope) {
         var ctrl = $controller('TodoCtrl', {$scope: $rootScope, localStorageService: localStorage});
 
+        expect($rootScope.canEditTodo()).toBe(false);
+    }));
+    
+    it('Should check rights to remove item', inject(function ($controller, $rootScope) {
+        var ctrl = $controller('TodoCtrl', {$scope: $rootScope, localStorageService: localStorage});
+
+        expect($rootScope.canRemoveTodo()).toBe(false);
+    }));
+    
+    it('Should check rights to check item', inject(function ($controller, $rootScope) {
+        var ctrl = $controller('TodoCtrl', {$scope: $rootScope, localStorageService: localStorage});
+
+        expect($rootScope.canCheckTodo()).toBe(false);
+    }));
+    
+    it('Should mark item as done', inject(function ($controller, $rootScope) {
+        var ctrl = $controller('TodoCtrl', {$scope: $rootScope, localStorageService: localStorage});
+
+        expect($rootScope.markDone()).toBe(undefined);
+    }));
+    
+    it('Should change status in system', inject(function ($controller, $rootScope) {
+        var ctrl = $controller('TodoCtrl', {$scope: $rootScope, localStorageService: localStorage});
+        var role = $rootScope.roles.nurse;
+        
+        expect($rootScope.changeStatusInSystem(role)).toBe(role);
+    }));
+
+    it('Should add new item', inject(function ($controller, $rootScope) {
+        var ctrl = $controller('TodoCtrl', {$scope: $rootScope, localStorageService: localStorage});
+        var flag;
+        
+        runs(function() {
+            flag = false;
+            $rootScope.todoText = true;
+            
+            expect($rootScope.addNewTodo()).toBe(true);
+            
+            setTimeout(function() {
+                flag = true;
+            }, 500);
+        });
+        
+        waitsFor(function() {
+            $rootScope.todoText = false;
+            return flag;
+        }, "Input text should not be empty", 750);
+        
+        runs(function() {
+            expect($rootScope.addNewTodo()).toBe(false);
+        });
+    }));
+
+    it('Should get number of active items', inject(function ($controller, $rootScope) {
+        var ctrl = $controller('TodoCtrl', {$scope: $rootScope, localStorageService: localStorage});
+        var flag;
+        var count;
+        
+        runs(function() {
+            flag = false;
+            count = 0;
+            $rootScope.todoList = [{done: true}];
+            
+            expect($rootScope.getActiveTaskQuantity()).toBe(count);
+            
+            setTimeout(function() {
+                flag = true;
+            }, 500);
+        });
+        
+        waitsFor(function() {
+            count = 1;
+            $rootScope.todoList = [{done: false}];
+            return flag;
+        }, "Should be some active items", 750);
+        
+        runs(function() {
+            expect($rootScope.getActiveTaskQuantity()).toBe(count);
+        });
+    }));
+
+    it('Check func for clear done todos', inject(function ($controller, $rootScope) {
+        var ctrl = $controller('TodoCtrl', {$scope: $rootScope, localStorageService: localStorage});
+        $rootScope.todoList = [{done: true}];
+        
         $rootScope.todoText = '';
         expect($rootScope.clearDoneTodos()).toBeUndefined();
     }));
