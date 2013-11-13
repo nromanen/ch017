d075394e69bf3c8abf4d82a371bb0487ba1a47af
FostiyRuslan <rfostiy@ettera.com>
2013-11-13 22:29:45 +0200

    fix bug with updateTodo opportunity and change design a little

--- js/directive/todo-click.js
+++ js/directive/todo-click.js
@@ -1,12 +1,17 @@
 angular.module("todo-click", [])
-    .directive("click", function () {
+    .directive("dbclick", function () {
         return {
             restrict: "A",
             link: function (scope, elem, attrs) {
-                elem.bind('click', function () {
+                elem.bind('dblclick', function () {
                     scope.$apply(attrs.click);
-                    elem.hide();
-                    elem.next("form").show();
+
+                    if (scope.statusInSystem.rights.edit) {
+                        angular.element(".todo_item").show();
+                        angular.element(".edit-form").hide();
+                        elem.hide();
+                        elem.next("form").show().focus();
+                    }
                 });
             }
         }
