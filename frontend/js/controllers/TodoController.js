f6d3bd482053bb80552072a5c4a9c6b1ca5efa7d
Artur Dolgii <artur.dolgii@gmail.com>
2013-11-26 23:56:49 +0200

    Reworked activePatientList

--- frontend/js/controllers/TodoController.js
+++ frontend/js/controllers/TodoController.js
@@ -2,31 +2,15 @@
 App.controller("TodoController", function ($scope, $rootScope, localStorageService, $http, config) {
 
     $scope.roles = {};
-    $scope.todoList = [];
-
     
     $scope.activePatientList = [];
-    
-    
     
     init();
 
     function init() {
-         //list of todos
-        //$scope.todoList = localStorageService.get("todos_list") || [];
+    
+        $scope.allTodos = localStorageService.get("allTodos") || [];
 
-        
-        
-        
-        
-
-        $scope.allTodos = localStorageService.get("allTodos") || [[],[],[],[],[],[],[],[],[],[],[]];
-        
-        
-        
-        
-        
-        
         $scope.statusInSystem = localStorageService.get("statusInSystem");
 
         $rootScope.showTopPanel = true;
@@ -47,11 +31,62 @@ App.controller("TodoController", function ($scope, $rootScope, localStorageServi
         };
     }
 
-    
-    
-    
-    
-    
+    $scope.getTodos = (function() {
+        
+        if($scope.allTodos) return false;
+        
+        var url = config.serverUrl + config.apiUrl + 'todos/?callback=JSON_CALLBACK';
+
+        $http.jsonp(url).
+        success(function(data, status) {
+
+            /* *** I AM GETTING FROM DJANGO *** */
+            /*
+            data = {{An object of array of users with todo-list of objects.}};
+            */
+            
+            /* TEMP */
+            data = {
+                7: [
+                    {
+                        "date_finished": "2013-11-23T05:18:43", 
+                        "text": "todo n1", 
+                        "amount": 1, 
+                        "time": [
+                            {
+                                "time": "2013-11-23T05:18:04"
+                            }, 
+                            {
+                                "time": "2013-11-24T12:00:00"
+                            }
+                        ], 
+                        "date_created": "2013-11-23T05:18:52.505"
+                    },
+                    {
+                        "date_finished": "2013-11-23T05:18:43", 
+                        "text": "todo n2", 
+                        "amount": 1, 
+                        "time": [
+                            {
+                                "time": "2013-11-23T05:18:04"
+                            }, 
+                            {
+                                "time": "2013-11-24T12:00:00"
+                            }
+                        ], 
+                        "date_created": "2013-11-23T05:18:52.505"
+                    }
+                ]
+            };
+            
+            $scope.allTodos = data;
+            
+        }).
+        error(function(data, status) {
+            console.log('error');
+        });
+
+    })();
     
     $scope.getPatients = (function() {
 
@@ -77,27 +112,19 @@ App.controller("TodoController", function ($scope, $rootScope, localStorageServi
     })();
 
     //Sort function
-
-    function sortByAlphabet(personA, personB) { //sort patient's by alphabet
+    function sortByAlphabet(personA, personB) { //sort patients by alphabet
         return personA.first_name > personB.first_name;
     }
 
     //active patient functions
     
-    $scope.getActivePatient = function () { //get avtive patient (realy we don't need it now)
-        $scope.currentPatient = this.patient.name;
+    $scope.getActivePatient = function() { //get avtive patient (realy we don't need it now)
+        $scope.currentPatient = this.patient.first_name + ' ' + this.patient.last_name;
     }
     
-    $scope.setActivePatient = function(patientId) { //set activ patient
+    $scope.setActivePatient = function( patientId ) { //set activ patient
         $scope.activePatientList = $scope.allTodos[ patientId ];
     }
-    
-    
-    
-    
-    
-    
-    
     
     //function update data in local storage after each change
     $scope.updateLocalStorage = function() {
@@ -130,7 +157,21 @@ App.controller("TodoController", function ($scope, $rootScope, localStorageServi
     $scope.addNewTodo = function() {
         if (!$scope.todoText) return false;
 
-        var item = {text: $scope.todoText, done: false};
+        var item = {
+            "date_finished": "2013-11-23T05:18:43", 
+            "text": $scope.todoText, 
+            "amount": 1, 
+            "done": false,
+            "time": [
+                {
+                    "time": "2013-11-23T05:18:04"
+                }, 
+                {
+                    "time": "2013-11-24T12:00:00"
+                }
+            ], 
+            "date_created": "2013-11-23T05:18:52.505"
+        };
 
         $scope.activePatientList.push(item);
         $scope.todoText = '';
@@ -139,6 +180,8 @@ App.controller("TodoController", function ($scope, $rootScope, localStorageServi
     $scope.getActiveTaskQuantity = function() {
         var count = 0;
 
+        if(!$scope.activePatientList) return false;
+        
         $scope.activePatientList.forEach(function(todo) {
             if (!todo.done) {
                 ++count;
