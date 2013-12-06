43827b59e08ceb529db07a693529292d13555c32
FostiyRuslan <rfostiy@ettera.com>
2013-12-06 17:37:57 +0200

    add time.id to json obj

--- frontend/js/directive/edit.js
+++ frontend/js/directive/edit.js
@@ -7,6 +7,9 @@ App.directive('edit', ['$rootScope', function($rootScope) {
                     $rootScope.todoExample = scope.currentPatient.todo.filter(function (todo) {
                         return todo.id == attrs.todoItem;
                     })[0];
+                    $rootScope.todoExample.time = $rootScope.todoExample.time.filter(function (time) {
+                        return time.id == attrs.timeItem;
+                    });
                     $rootScope.todoExample.edit = true;
                 });
             });
