998164d2ba5db9c035810829c9c56537d5ec889d
FostiyRuslan <rfostiy@ettera.com>
2013-12-14 15:26:22 +0200

    refactor code of filters

--- frontend/js/filters/datetime.js
+++ frontend/js/filters/datetime.js
@@ -4,35 +4,28 @@ App.filter('morning', ['dayPart', function(dayPart) {
     return function(times,  $scope) {
         if (!times.length) return false;
 
-        var filteredTimes = times.filter(function(timeItem) {
+        return times.filter(function(timeItem) {
             return timeItem.time.split(':')[0] < dayPart.noon &&
                 timeItem.time.split(':')[0] >= dayPart.morning;
         });
-
-        return filteredTimes;
-
     };
 }]).filter('noon', ['dayPart', function(dayPart) {
 
     return function(times, $scope) {
         if (!times.length) return false;
 
-        var filteredTimes = times.filter(function(timeItem) {
+        return times.filter(function(timeItem) {
             return timeItem.time.split(':')[0] >= dayPart.noon &&
                 timeItem.time.split(':')[0] < dayPart.evening;
         });
-
-        return filteredTimes;
     };
 }]).filter('evening', ['dayPart', function(dayPart) {
 
     return function(times, $scope) {
         if (!times.length) return false;
 
-        var filteredTimes = times.filter(function(timeItem) {
+        return times.filter(function(timeItem) {
             return timeItem.time.split(':')[0] >= dayPart.evening;
         });
-
-        return filteredTimes;
     };
 }]);
