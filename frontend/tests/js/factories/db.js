490183d44d1be0353a38c67907f08064d9894f9b
Artur Dolgii <artur.dolgii@gmail.com>
2014-01-12 21:52:26 +0200

    Code coverage increased.

--- frontend/tests/js/factories/db.js
+++ frontend/tests/js/factories/db.js
@@ -52,24 +52,42 @@ describe('Database factory', function() {
 
      beforeEach(inject(function($injector) {
         $httpBackend = $injector.get('$httpBackend');
-        $httpBackend.when('GET', 'api/user/doctor/MTExMQ==/').respond({role: {name: 'Doctor'}, login: 'doctor'});
-        $httpBackend.when('GET', 'api/users_by_role/patient/').respond([1, 2, 3]);
-        $httpBackend.when('POST', 'api/create_todo/1/').respond({});
-        $httpBackend.when('PUT', 'api/update_todo/1/1/').respond({});
-        $httpBackend.when('DELETE', 'api/delete_todo/1/1/').respond({});
+        $httpBackend.when('GET', 'api/user/doctor/MTExMQ==/');
+        $httpBackend.when('GET', 'api/users_by_role/patient/');
+        $httpBackend.when('POST', 'api/create_todo/1/');
+        $httpBackend.when('PUT', 'api/update_todo/1/1/');
+        $httpBackend.when('DELETE', 'api/delete_todo/1/1/');
+        $httpBackend.when('GET', 'api/medicines/');
     }));
 
     it('Should get user data', inject(function($rootScope) {
         var login = $rootScope.currentUser.login;
         var password = $rootScope.currentUser.password;
+        var flag;
+        var flag1;
+        var user = $rootScope.currentUser;
 
-        $httpBackend.expectGET('api/user/doctor/MTExMQ==/');
+        user.result = true;
+        $httpBackend.expectGET('api/user/doctor/MTExMQ==/').respond(201, user);
+        db.getUserData(login, password);
+
+        user.result = false;
+        $httpBackend.expectGET('api/user/doctor/MTExMQ==/').respond(201, user);
+        db.getUserData(login, password);
+
+        user.is_staff = false;
+        $httpBackend.expectGET('api/user/doctor/MTExMQ==/').respond(201, user);
+        db.getUserData(login, password);
+
+        $httpBackend.expectGET('api/user/doctor/MTExMQ==/').respond(404, {});
         db.getUserData(login, password);
         $httpBackend.flush();
     }));
 
     it('Should get patient list', inject(function($rootScope) {
-        $httpBackend.expectGET('api/users_by_role/patient/');
+        $httpBackend.expectGET('api/users_by_role/patient/').respond(201, [1, 2, 3]);
+        db.getPatientList($rootScope.currentUser);
+        $httpBackend.expectGET('api/users_by_role/patient/').respond(404, {});
         db.getPatientList($rootScope.currentUser);
         $httpBackend.flush();
     }));
@@ -78,7 +96,9 @@ describe('Database factory', function() {
         var id = $rootScope.currentPatient.id;
         var object = $rootScope.currentPatient.todo;
 
-        $httpBackend.expectPOST('api/create_todo/1/');
+        $httpBackend.expectPOST('api/create_todo/1/').respond(201, '');
+        db.addTodo(id, object);
+        $httpBackend.expectPOST('api/create_todo/1/').respond(404, '');
         db.addTodo(id, object);
         $httpBackend.flush();
     }));
@@ -86,7 +106,9 @@ describe('Database factory', function() {
     it('Should edit todo', inject(function($rootScope) {
         var object = $rootScope.currentPatient;
 
-        $httpBackend.expectPUT('api/update_todo/1/1/');
+        $httpBackend.expectPUT('api/update_todo/1/1/').respond(201, '');
+        db.editTodo(object);
+        $httpBackend.expectPUT('api/update_todo/1/1/').respond(404, '');
         db.editTodo(object);
         $httpBackend.flush();
     }));
@@ -94,8 +116,18 @@ describe('Database factory', function() {
     it('Should delete todo', inject(function($rootScope) {
         var id = $rootScope.currentPatient.todo[0].id;
 
-        $httpBackend.expectDELETE('api/delete_todo/1/1/');
+        $httpBackend.expectDELETE('api/delete_todo/1/1/').respond(201, '');
         db.deleteTodo(id);
+        $httpBackend.expectDELETE('api/delete_todo/1/1/').respond(404, '');
+        db.deleteTodo(id);
+        $httpBackend.flush();
+    }));
+
+    it('Should get medicines', inject(function($rootScope) {
+        $httpBackend.expectGET('api/medicines/').respond(201, '');
+        db.getMedicines();
+        $httpBackend.expectGET('api/medicines/').respond(404, '');
+        db.getMedicines();
         $httpBackend.flush();
     }));
 });
