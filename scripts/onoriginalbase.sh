0690cab722526d2ab5597ebae402754885ab6b05
TarasKolba <tarix2006@gmail.com>
2014-01-16 01:55:02 +0200

    script rollover mogo for test

--- scripts/onoriginalbase.sh
+++ scripts/onoriginalbase.sh
@@ -2,4 +2,4 @@
 export SETTING_DJANGO=../backend/app_serverside/settings.py
 export SETTING_NODE=../server/model/mongodb.js
 sed -i -e "s/'NAME': 'todos_test'/'NAME': 'todos'/" $SETTING_DJANGO
-sed -i -e "s/mongodb:\/\/localhost:27017\/todoDb_test/mongodb:\/\/localhost:27017\/todoDb/" $SETTING_NODE
+sed -i -e "s/'mongodb:\/\/localhost:27017\/todoDb_test'/'mongodb:\/\/localhost:27017\/todoDb'/" $SETTING_NODE
--- scripts/ontestbase.sh
+++ scripts/ontestbase.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 export SETTING_DJANGO=../backend/app_serverside/settings.py
 export SETTING_NODE=../server/model/mongodb.js
+export MEDICINES=../dumps/mongodb/medicines.json
+export ROLES=../dumps/mongodb/roles.json
+export USERS=../dumps/mongodb/users.json
 sed -i -e "s/'NAME': 'todos'/'NAME': 'todos_test'/" $SETTING_DJANGO
-sed -i -e "s/mongodb:\/\/localhost:27017\/todoDb/mongodb:\/\/localhost:27017\/todoDb_test/" $SETTING_NODE
+sed -i -e "s/'mongodb:\/\/localhost:27017\/todoDb'/'mongodb:\/\/localhost:27017\/todoDb_test'/" $SETTING_NODE
+
+mongoimport --db todoDb_test --collection medicines --file $MEDICINES --jsonArray
+mongoimport --db todoDb_test --collection roles --file $ROLES --jsonArray
+mongoimport --db todoDb_test --collection users --file $USERS --jsonArray
