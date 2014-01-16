a1b21cba214a5db6af9f82a4d503e9a9070e0129
Taras <tarix2006@gmail.com>
2014-01-16 14:54:40 +0200

    some edit .sh

--- scripts/ontestbase.sh
+++ scripts/ontestbase.sh
@@ -4,9 +4,11 @@ export SETTING_NODE=../server/model/mongodb.js
 export MEDICINES=../dumps/mongodb/medicines.json
 export ROLES=../dumps/mongodb/roles.json
 export USERS=../dumps/mongodb/users.json
+export SQLSCRIPT=../dumps/todos_test.sql 
 sed -i -e "s/'NAME': 'todos'/'NAME': 'todos_test'/" $SETTING_DJANGO
 sed -i -e "s/'mongodb:\/\/localhost:27017\/todoDb'/'mongodb:\/\/localhost:27017\/todoDb_test'/" $SETTING_NODE
 
 mongoimport --db todoDb_test --collection medicines --file $MEDICINES --jsonArray
 mongoimport --db todoDb_test --collection roles --file $ROLES --jsonArray
 mongoimport --db todoDb_test --collection users --file $USERS --jsonArray
+echo psql -U developer -d todos_test -a -f $SQLSCRIPT
