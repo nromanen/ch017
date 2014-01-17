f6b78e75b91603bfe425c6a18cf4e4da17ab92a8
Danylo_Mysko <veneomin01@gmail.com>
2014-01-17 17:18:39 +0200

    Aded validate for buttons

--- backbone/main.js
+++ backbone/main.js
@@ -47,10 +47,8 @@
             this.collection.each(function(person) {
 		   
                 var questionView = new App.Views.Question({model: person});
- 
                 this.$el.append(questionView.render().el);
-            }, this);
-			
+           }, this);
             return this;
         }
  
@@ -65,7 +63,7 @@
         this.model.on('change', this.render, this); 
 		
 		this.model.on('change', function(){
-		console.log('1');
+		//console.log('1');
 		});
 		
         this.render();
@@ -90,21 +88,34 @@
  
         initialize: function() {
             this.render();
+			this.model.on('change', this.render, this);
         },
 		
 		events:{
 		'click .correct': 'setCorrect',
 		'click .incorrect': 'setIncorrect'
 		},
-		
 		setCorrect: function(){
+		var status = this.model.get('correct');
+			if (status != 'not answered yet'){
+				return
+			}
+			
 		var countTrue = counterModel.get('correct' )+ 1;
 			counterModel.set('correct', countTrue);
+			this.model.set('correct', 'correct')
 		},
 		
 		setIncorrect: function(){
+			var status = this.model.get('correct');
+				if (status != 'not answered yet'){
+					return
+			}
+		
+		this.model.set('correct', 'incorrect');
+
 		var countFalse = counterModel.get('incorrect' )+ 1;
-			counterModel.set('incorrect', countFalse);		
+			counterModel.set('incorrect', countFalse);				
 		},
  
         render: function() {
@@ -118,31 +129,45 @@
  //колекция вопросов
     var questionCollection = new App.Collections.Questions([
         {
-           text: 'Lorem ipsum dolor sit amet'
+           text: 'Lorem ipsum dolor sit amet',
+		   correct: 'not answered yet',
+        },
+		  {
+           text: 'Sed purus neque, faucibus ac leo non.',
+		   correct: 'not answered yet',
         },
         {
-           text: 'Consectetur adipiscing elit.'
+           text: 'Consectetur adipiscing elit.',
+		   correct: 'not answered yet',
         },
 		  {
-           text: 'Volutpat purus at, accumsan enim.'
+           text: 'Volutpat purus at, accumsan enim.',
+		   correct: 'not answered yet',
         },
 		  {
-           text: 'Aenean rhoncus eros id dui posuere cursus.'
+           text: 'Sed purus neque, faucibus ac leo non.',
+		   correct: 'not answered yet',
         },
 		  {
-           text: 'Sed purus neque, faucibus ac leo non.'
+           text: 'Aenean rhoncus eros id dui posuere cursus.',
+		   correct: 'not answered yet',
+        },
+		  {
+           text: 'Sed purus neque, faucibus ac leo non.',
+		   correct: 'not answered yet',
         },
         {
-           text: 'Cras id sapien consectetur'
+           text: 'Cras id sapien consectetur',
+		   correct: 'not answered yet',
         }
     ]);
  
- 
+	
     var questionsView = new App.Views.Questions({collection: questionCollection});
 	var counterModel = new App.Models.Counter();
 	var counterView = new App.Views.Counter({model: counterModel});
  
     $(document.body).append(questionsView.render().el);
-	$(document.body).append(counterView.render().el);
- 
+	$(document.body).append(counterView.el);
+ console.log(questionCollection.at(1))
 }());
