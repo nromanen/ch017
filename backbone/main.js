3c2af763ab9b09621d1eb6317852d3cd8a0f95d1
Danylo_Mysko <veneomin01@gmail.com>
2014-01-27 18:03:42 +0200

    Refactored code, change name of some components, create model for index

--- backbone/main.js
+++ backbone/main.js
@@ -19,29 +19,38 @@
 	  },
 	
         defaults: {
-            text: 'Type your question there'
+            text: '',
+			status: 'not answered yet'
         }
     });
 	
-	//Модель счетчика
-	App.Models.Counter = Backbone.Model.extend({
+	//Модель индекса
+	App.Models.Index = Backbone.Model.extend({
 	
-	  validate: function(attrs, options) {
+	validate: function(attrs, options) {
 		if (attrs.index < 0 || attrs.index > questionCollection.length - 1) {
-      return 'error';
-	  }    
+			return 'error';
+		}    
+	},
 	
-  },
- 
+		defaults: {
+            index: 0
+        }
+	});
+	
+	var indexModel = new App.Models.Index();
+	
+	//Модель статистики
+	App.Models.Statistic = Backbone.Model.extend({
 		defaults: {
 		correct: 0,
 		incorrect: 0,
 		notAnswered:0,
-		index:0		
+		//index:0		
 		}
 	});
 	
-	var counterModel = new App.Models.Counter();
+	var statisticModel = new App.Models.Statistic();
  
     //Список вопросов
     App.Collections.Questions = Backbone.Collection.extend({
@@ -56,8 +65,8 @@
         initialize: function() {
 		
 		this.collection.on('change', function(){
-		var index = counterModel.get('index')
-		counterModel.set({index: index + 1},{validate:true})
+			var index = indexModel.get('index')
+			indexModel.set({index: index + 1},{validate:true})
 		}, this);
 			
 		this.collection.on('change', this.render, this);
@@ -65,7 +74,7 @@
 		this.render();
         },
 		
-				events:{
+				events:{ //proxy
 		'click .correct': 'setCorrect',
 		'click .incorrect': 'setIncorrect',
 		'click .forward': 'indexIncrease',
@@ -73,58 +82,58 @@
 		},
 		
 		setCorrect: function(){
-			var status = this.collection.at(counterModel.get('index')).get('correct');
+			var status = this.collection.at(indexModel.get('index')).get('status');
 				if (status != 'not answered yet'){
 					return
 				}
 			
-		this.collection.at(counterModel.get('index')).set('correct', 'correct')
-			counterModel.set('correct', counterModel.get('correct' )+ 1);
-			counterModel.set({notAnswered: counterModel.get('notAnswered') - 1 });
+		this.collection.at(indexModel.get('index')).set('status', 'correct')
+			statisticModel.set('correct', statisticModel.get('correct' )+ 1);
+			statisticModel.set({notAnswered: statisticModel.get('notAnswered') - 1 });
 		},
 		
 		setIncorrect: function(){
-			var status = this.collection.at(counterModel.get('index')).get('correct');
+			var status = this.collection.at(indexModel.get('index')).get('correct');
 				if (status != 'not answered yet'){
 					return
-			}
+		}
 		
-		this.collection.at(counterModel.get('index')).set('correct', 'incorrect');
-			counterModel.set({incorrect: counterModel.get('incorrect' )+ 1});
-			counterModel.set({notAnswered: counterModel.get('notAnswered') - 1 });
+		this.collection.at(indexModel.get('index')).set('status', 'incorrect');
+			statisticModel.set({incorrect: statisticModel.get('incorrect' )+ 1});
+			statisticModel.set({notAnswered: statisticModel.get('notAnswered') - 1 });
 
 		},
 		
 		indexIncrease: function(){
-			var index = counterModel.get('index')
-			counterModel.set({index: index + 1},{validate:true})
+			var index = indexModel.get('index')
+			indexModel.set({index: index + 1},{validate:true})
 			this.render();
 		},
 		
 		indexDecrease: function(){
-			var index = counterModel.get('index')
-			counterModel.set({index: index - 1},{validate:true})
+			var index = indexModel.get('index')
+			indexModel.set({index: index - 1},{validate:true})
 			this.render();
 		},
 		
 		template: template('main-id'),
 		
         render: function() {
-			this.$el.html( this.template( this.collection.at(counterModel.get('index')).toJSON() ) );
+			this.$el.html( this.template( this.collection.at(indexModel.get('index')).toJSON() ) );
 			return this;
         }
  
     });
  
 	//Вид счетчика
-	App.Views.Counter = Backbone.View.extend({
+	App.Views.Statistic = Backbone.View.extend({
 	tagName: 'li',
 	className: 'noListingStyle',
 	initialize: function() {  
 		  		  
         this.model.on('change', this.render, this); 
 		
-		counterModel.set({notAnswered:questionCollection.length})
+		statisticModel.set({notAnswered:questionCollection.length})
 		
         this.render();
         },
@@ -143,7 +152,7 @@
            text: 'Lorem ipsum dolor sit amet',
 		   correct: 'not answered yet',
         },
-		  {
+		{
            text: 'Sed purus neque, faucibus ac leo non.',
 		   correct: 'not answered yet',
         },
@@ -151,19 +160,27 @@
            text: 'Consectetur adipiscing elit.',
 		   correct: 'not answered yet',
         },
-		  {
+		{
            text: 'Volutpat purus at, accumsan enim.',
 		   correct: 'not answered yet',
         },
-		  {
+		{
            text: 'Sed purus neque, faucibus ac leo non.',
 		   correct: 'not answered yet',
         },
-		  {
+		{
            text: 'Aenean rhoncus eros id dui posuere cursus.',
 		   correct: 'not answered yet',
         },
-		  {
+		{
+           text: 'Sed purus neque, faucibus ac leo non.',
+		   correct: 'not answered yet',
+        },
+		{
+           text: 'Sed purus neque, faucibus ac leo non.',
+		   correct: 'not answered yet',
+        },
+		{
            text: 'Sed purus neque, faucibus ac leo non.',
 		   correct: 'not answered yet',
         },
@@ -175,9 +192,9 @@
 
 	var questionModel = new App.Models.Question();
     var questionsView = new App.Views.Questions({collection: questionCollection});
-	var counterModel = new App.Models.Counter();
-	var counterView = new App.Views.Counter({model: counterModel});
-	
+	var statisticModel = new App.Models.Statistic();
+	var statisticView = new App.Views.Statistic({model: statisticModel}); 
+
     $(document.body).append(questionsView.el);
-	$(document.body).append(counterView.el);
+	$(document.body).append(statisticView.el);
 }());
